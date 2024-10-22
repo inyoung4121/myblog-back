0568dc3e5004c5fcafc05cf570f60c5dba40a6e5
inyoung4121 <inyoungyang01@gmail.com>
2024-10-22 11:59:25 +0900

    refactor: 쿼리 캐싱 추가

--- src/main/java/in/myblog/sidebar/SidebarService.java
+++ src/main/java/in/myblog/sidebar/SidebarService.java
@@ -1,56 +1,22 @@
 package in.myblog.sidebar;
 
-import in.myblog.post.domain.Tags;
-import in.myblog.post.domain.TotalVisitCount;
-import in.myblog.post.repository.TagRepository;
-import in.myblog.post.repository.TotalVisitCountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Service
 public class SidebarService {
 
-    private final TotalVisitCountRepository totalVisitCountRepository;
-    private final TagRepository tagRepository;
-
-    @Autowired
-    public SidebarService(TotalVisitCountRepository totalVisitCountRepository, TagRepository tagRepository) {
-        this.totalVisitCountRepository = totalVisitCountRepository;
-        this.tagRepository = tagRepository;
-    }
+    private final SidebarCacheService sidebarCacheService;
 
     public SidebarDataDTO getSidebarData() {
-        VisitorCountsDTO visitorCounts = getVisitorCounts();
-        List<String> tags = getTags();
+        VisitorCountsDTO visitorCounts = sidebarCacheService.getVisitorCounts();
+        List<String> tags = sidebarCacheService.getTags();
 
         return new SidebarDataDTO(visitorCounts, tags);
     }
 
-    @Cacheable(value = "visitCounts")
-    public VisitorCountsDTO getVisitorCounts() {
-        LocalDate today = LocalDate.now();
-        LocalDate yesterday = today.minusDays(1);
 
-        long totalCount = totalVisitCountRepository.count();
-        long todayCount = totalVisitCountRepository.findByDate(today)
-                .map(TotalVisitCount::getCount)
-                .orElse(0L);
-        long yesterdayCount = totalVisitCountRepository.findByDate(yesterday)
-                .map(TotalVisitCount::getCount)
-                .orElse(0L);
-
-        return new VisitorCountsDTO(totalCount, todayCount, yesterdayCount);
-    }
-
-    @Cacheable(value = "tags")
-    public List<String> getTags() {
-        return tagRepository.findAll().stream()
-                .map(Tags::getName)
-                .collect(Collectors.toList());
-    }
 }
